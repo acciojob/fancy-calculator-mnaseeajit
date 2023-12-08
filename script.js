af542be0989d2c5ba8cb540d97658b9a6bca234a
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-08 13:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function ans(input1 , input2){
-	return ("The final result is : "+(parseInt(input1 * 2) + parseInt(input2)));
+	return ("The final result is: "+(parseInt(input1 * 2) + parseInt(input2)));
 }
  let input1 = prompt("enter value1");
  let input2 = prompt("enter value2");
